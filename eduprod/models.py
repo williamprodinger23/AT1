b9ffd03c3341db40fd3f86a6ee06fed9b0d83a28
williamprodinger <william.prodinger@student.uts.edu.au>
2024-04-04 21:12:39 +1100

    final commit

--- eduprod/models.py
+++ eduprod/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Question(models.Model):
+    #Create question and answer fields in database.
     question = models.CharField(max_length=150)
     answer_1 = models.TextField(max_length=150, null=True)
     answer_2 = models.TextField(max_length=150, null=True)
     answer_3 = models.TextField(max_length=150, null=True)
     answer_4 = models.TextField(max_length=150, null=True)
 
-    NUMBER_CHOICES = {1:1,2:2,3:3,4:4}
+    NUMBER_CHOICES = {1:1,2:2,3:3,4:4} #Set options for correct answer field.
     correct_Answer = models.IntegerField(null = False, choices = NUMBER_CHOICES)
 
     def __str__(self):
--- users/views.py
+++ users/views.py
@@ -6,41 +6,53 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 
 def index(request):
+    #if user is not authenticated take to login page
+    #if user is authenticated take to user page
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("users:login"))
     return render(request, "users/user.html")
 
 def login_view(request):
+    #Check user for authentication with given username and password.
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
+            #User is authenticated
+            #Log user in with build in Django Function
+            #Take user to quiz app
             login(request, user)
             return HttpResponseRedirect(reverse("eduprod:index"))
         else:
+            #User is not authenticated, take to login page
             messages.success(request, "Invalid Credentials.")
             return render(request, "users/login.html")
     return render(request, "users/login.html")
 
 def logout_view(request):
+    #Run built in Django Logout Function .
+    #return message saying success.
+    #Take user to login page.
     logout(request)
     messages.success(request, "Successfully logged out.")
     return redirect(reverse('users:login'))
 
 def signup_view(request):
-    return render(request, "users/signup.html")
+    return render(request, "users/signup.html") #Take User To Signup Page.
 
 def signup_run(request):
+    #Create User With Submitted Username And Password
     if request.method == "POST":
         if request.POST["username"]:
             username = request.POST["username"]
         if request.POST["password"]:
             password = request.POST["password"]
-        user = User.objects.create_user(username, '', password) #Create User With Submitted Username And Password
+        user = User.objects.create_user(username, '', password)
         user.save()
     return HttpResponseRedirect(reverse("eduprod:index"))
     
 def data_change(request):
+    #Take user to admin page. (Database)
     return render(request, "admin")
 
